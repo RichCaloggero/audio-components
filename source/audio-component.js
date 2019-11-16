fc22fe7fc359c357fedb7cd96368d2a76045d9c8
Rich <rjc@mit.edu>
2019-11-15 21:55:54 -0500

    fixed audio-split.js and split.js
    - because swap attributes not passed sown to component, defined a connect() method on Split instances which must be called to establish the connection
    - called with arguments swapInputs and swapOutputs, respectively
    - Polymer does not convert "atribute-foo" to camel case as described in the manual; you get a property on the instance named "attribute-foo"

--- source/audio-component.js
+++ source/audio-component.js
@@ -56,32 +56,36 @@ this.wet.gain.value = gain;
 export class Split extends AudioComponent {
 constructor (audio, components) {
 super (audio, "split");
-const s = this.audio.createChannelSplitter(2);
-const m = this.audio.createChannelMerger(2);
+this.splitter = this.audio.createChannelSplitter(2);
+this.merger = this.audio.createChannelMerger(2);
+this.components = components;
 
 if (components.length === 0 || components.length > 2) {
 alert("Split: must have at least one, and no more than two child elements");
 return;
 } // if
 
-const channel1 = components[0];
-const channel2 = components.length === 1? null : components[1];
+this.input.connect(this.splitter);
+this.merger.connect(this.wet);
+} // constructor
 
+connect (swapInputs, swapOutputs) {
+const channel1 = this.components[0];
+const channel2 = this.components.length === 1? null : this.components[1];
+
+alert (`split: swap: ${swapInputs}, ${swapOutputs}`);
 if (channel1) {
-s.connect (channel1.input, this.swapInputs? 1 : 0, 0);
-channel1.output.connect (m, 0, this.swapOutputs? 1 : 0);
+this.splitter.connect (channel1.input, swapInputs? 1 : 0, 0);
+channel1.output.connect (this.merger, 0, swapOutputs? 1 : 0);
 console.log(`- channel 1: ${channel1.name} connected`);
 } // if
 
 if (channel2) {
-s.connect (channel2.input, this.swapInputs? 0 : 1, 0);
-channel2.output.connect (m, 0, this.swapOutputs? 0 : 1);
+this.splitter.connect (channel2.input, swapInputs? 0 : 1, 0);
+channel2.output.connect (this.merger, 0, swapOutputs? 0 : 1);
 console.log(`- channel 2: ${channel2.name} connected`);
 } // if
-
-this.input.connect(s);
-m.connect(this.wet);
-} // constructor
+} // connect
 } // class Split
 
 export class Series extends AudioComponent {
