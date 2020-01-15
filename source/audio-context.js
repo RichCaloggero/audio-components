5902b61b8b935d6e3d3c6646d9d316f9e55a5e91
Rich <rjc@mit.edu>
2020-01-15 10:15:46 -0500

    * fixed mispelling of "analyser"

--- source/audio-context.js
+++ source/audio-context.js
@@ -43,7 +43,7 @@ return html`
 <legend><h1>[[label]]</h1></legend>
 <ui-boolean label="enable automation" value="{{enableAutomation}}" shortcut="alt shift a"></ui-boolean>
 <ui-number label="automationInterval" value="{{automationInterval}}" min="0.01" max="3.0" step="0.01"></ui-number>
-<ui-boolean label="enable analyzer" value="{{enableAnalyzer}}" shortcut="alt shift x"></ui-boolean>
+<ui-boolean label="enable analyser" value="{{enableAnalyser}}" shortcut="alt shift x"></ui-boolean>
 
 
 <ui-boolean label="showListener" value="{{showListener}}"></ui-boolean>
@@ -109,7 +109,7 @@ mix: {type: Number, notify: true, observer: "_mix"},
 bypass: {type: Boolean, notify: true, observer: "_bypass"},
 "silent-bypass": {type: Boolean, notify: true, observer: "_silentBypass"},
 enableAutomation: {type: Boolean, value: false, notify: true, observer: "_enableAutomation"}, // enableAutomation
-enableAnalyzer: {type: Boolean, value: false, notify: true, observer: "_enableAnalyzer"},
+enableAnalyser: {type: Boolean, value: false, notify: true, observer: "_enableAnalyser"},
 showListener: {type: Boolean, value: false, notify: true, observer: "_showListener"},
 recordMode: {type: Boolean, value: false, notify: true, observer: "_recordMode"},
 shortcuts: {type: String, notify:true, observer: "shortcutsChanged"},
@@ -147,7 +147,7 @@ audio = new AudioContext({sampleRate: 88200});
 } // if
 
 this.audio = audio;
-this.analyzer = audio.createAnalyzerNode();
+this.analyser = null;
 } // constructor
 
 
@@ -258,15 +258,16 @@ this.dispatchEvent(new CustomEvent("stopAutomation"));
 } // if
 } // _enableAutomation
 
-_enableAnalyzer (value) {
+_enableAnalyser (value) {
 if (audioPlayer) {
 if (value) {
-audioPlayer.output.connect(this.analyzer);
+this.analyser = new Analyser(audio);
+audioPlayer.output.connect(this.analyser);
 } else {
-audioPlayer.output.disconnect(this.analyzer);
+this.analyser = null;
 } // if
 } // if
-} // _enableAnalyzer
+} // _enableAnalyser
 
 _showListener (value) {if (shadowRoot) shadowRoot.querySelector("#listener").hidden = !value;}
 
